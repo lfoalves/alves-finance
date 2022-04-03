c3dcbf2088670913b7596f093890e29293407ca7
lfoalves <lfoalves.dev@gmail.com>
2022-04-03 10:29:01 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const route = require('./route')
+
+const server = express()
+
+server.set('view engine', 'ejs')
+server.use(express.static('public'))
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
+
+server.use(route)
+
+server.listen(3000, () => console.log('Server is Running'))
